8851ac03ea2c837f44c8540cdf06f18e9cdfe0a6
piezo <piezo5go@gmail.com>
2025-02-19 12:11:00 +0300

    I have added the ability to delete a post that at some instance existed.

--- blog2/urls.py
+++ blog2/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import BlogListView, PostDetailViiew, BlogCreateView, BlogEditView
+from .views import BlogListView, PostDetailViiew, BlogCreateView, BlogEditView, BlogDeleteView
 
 urlpatterns = [
+    path('post/<int:pk>/delete', BlogDeleteView.as_view(), name='post_delete'),
     path('post/<int:pk>/edit', BlogEditView.as_view(), name='post_edit'),
     path('post/new/', BlogCreateView.as_view(), name='post_new'),
     path('', BlogListView.as_view(), name='home' ),
